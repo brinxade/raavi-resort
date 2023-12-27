a71edaf0f1cba3e3cf17a5c1c032f92d206c72a4
Gurparsad Bajwa <brinxade@gmail.com>
2023-12-27 12:00:06 -0500

    Responsiveness updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,31 @@
 import { Link, NavLink } from "react-router-dom";
 import imgHeaderExt from "images/waves-themed-animated.svg";
 import logo from "images/logo.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Header(props) {
 
     const [menuIsOpen, setMenuIsOpen] = useState(false);
+    const [headerResponsive, setHeaderResponsive] = useState(false);
 
     const activeCheck = ({ isActive, isPending }) => 
     isPending ? "pending" : isActive ? "active" : "";
 
+    const onResize = () => {
+        if(window.innerWidth < 800) {
+            setHeaderResponsive(true);
+        } else {
+            setHeaderResponsive(false);
+        }
+    };
+
+    useEffect(() => {
+        onResize();
+        window.addEventListener("resize", onResize, true);
+    }, []);
+
     return (
-        <header id="site-header" className={`${props.hasHeroContainer && "header-transparent"} ${props.hasWaterEffect && "water-effect"} ${!menuIsOpen && "menu-collapsed"}`}>
+        <header id="site-header" className={`${props.hasHeroContainer && "header-transparent"} ${props.hasWaterEffect && "water-effect"} ${!menuIsOpen && "menu-collapsed"} ${headerResponsive && "header-responsive"}`}>
             <div className="inner ss-3">
                 <button id="header-toggle" className={`${menuIsOpen && "active"}`} onClick={() => setMenuIsOpen(!menuIsOpen)}><i className="fa-solid fa-bars"></i></button>
                 <Link to="/" className="logo">
